80038a54e535355912566394ee829c45ee06b6e8
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-03-05 22:12:09 +0200

    int Random Range

--- flutter/lib/commons/random.dart
+++ flutter/lib/commons/random.dart
@@ -7,3 +7,7 @@ double doubleRandomRange(double min, double max) {
   return source.nextDouble() * (max - min) + min;
 }
 
+/// Generate random int in range of min (inclusive) to max (exclusive)
+int intRandomRange(int min, int max) {
+  return source.nextInt(max - min) + min;
+}
