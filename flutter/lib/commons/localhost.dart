412e4f3a261dfd8fd2bc192892406093c0262286
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-03-05 22:13:11 +0200

    _internal

--- flutter/lib/commons/localhost.dart
+++ flutter/lib/commons/localhost.dart
@@ -5,3 +5,7 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 LocalhostManager get localhostManager {
   var instance = LocalhostManager._instance;
   if (instance == null) {
+    instance = LocalhostManager._internal();
+    LocalhostManager._instance = instance;
+  }
+
