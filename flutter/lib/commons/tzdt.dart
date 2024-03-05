4f607afee2f5c12aed69adccb195bc2763c13d2c
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-03-05 22:06:46 +0200

    tzDate

--- flutter/lib/commons/tzdt.dart
+++ flutter/lib/commons/tzdt.dart
@@ -41,3 +41,14 @@ TZDateTime tzToday([
     microsecond,
   );
 }
+
+TZDateTime tzDate(
+  int year, [
+  int month = 1,
+  int day = 1,
+  int hour = 0,
+  int minute = 0,
+  int second = 0,
+  int millisecond = 0,
+  int microsecond = 0,
+]) {
