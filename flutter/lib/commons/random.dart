5bf9843c73ef949cee0b9081ea6be4c2a3d56c30
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-03-05 22:11:58 +0200

    double Random Range

--- flutter/lib/commons/random.dart
+++ flutter/lib/commons/random.dart
@@ -2,3 +2,8 @@ import 'dart:math';
 
 Random source = Random();
 
+/// Generate random double in range of min (inclusive) to max (exclusive)
+double doubleRandomRange(double min, double max) {
+  return source.nextDouble() * (max - min) + min;
+}
+
