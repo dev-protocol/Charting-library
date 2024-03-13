5424a72bd74185e66aa3dd487fa032254d8bdf5d
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-03-13 23:57:32 +0200

    Create tvchart_types.dart

---
+++ flutter/lib/components/tvchart/tvchart_types.dart
@@ -0,0 +1,225 @@
+import 'package:flutter/foundation.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'tvchart_types.g.dart';
+
+enum ChartTheme {
+  @JsonValue('Light')
+  light,
+
+  @JsonValue('Dark')
+  dark,
+}
+
+enum AccessListType {
+  @JsonValue('black')
+  black,
+
+  @JsonValue('white')
+  white,
+}
+
+enum Timezone {
+  @JsonValue('Etc/UTC')
+  utc,
+
+  @JsonValue('Africa/Cairo')
+  africaCairo,
+
+  @JsonValue('Africa/Johannesburg')
+  africaJohannesburg,
+
+  @JsonValue('Africa/Lagos')
+  africaLagos,
+
+  @JsonValue('America/Argentina/Buenos_Aires')
+  americaArgentinaBuenosAires,
+
+  @JsonValue('America/Bogota')
+  americaBogota,
+
+  @JsonValue('America/Caracas')
+  americaCaracas,
+
+  @JsonValue('America/Chicago')
+  americaChicago,
+
+  @JsonValue('America/El_Salvador')
+  americaElSalvador,
+
+  @JsonValue('America/Juneau')
+  americaJuneau,
+
+  @JsonValue('America/Lima')
+  americaLima,
+
+  @JsonValue('America/Los_Angeles')
+  americaLosAngeles,
+
+  @JsonValue('America/Mexico_City')
+  americaMexicoCity,
+
+  @JsonValue('America/New_York')
+  americaNewYork,
+
+  @JsonValue('America/Phoenix')
+  americaPhoenix,
+
+  @JsonValue('America/Santiago')
+  americaSantiago,
+
+  @JsonValue('America/Sao_Paulo')
+  americaSaoPaulo,
+
+  @JsonValue('America/Toronto')
+  americaToronto,
+
+  @JsonValue('America/Vancouver')
+  americaVancouver,
+
+  @JsonValue('Asia/Almaty')
+  asiaAlmaty,
+
+  @JsonValue('Asia/Ashkhabad')
+  asiaAshkhabad,
+
+  @JsonValue('Asia/Bahrain')
+  asiaBahrain,
+
+  @JsonValue('Asia/Bangkok')
+  asiaBangkok,
+
+  @JsonValue('Asia/Chongqing')
+  asiaChongqing,
+
+  @JsonValue('Asia/Dubai')
+  asiaDubai,
+
+  @JsonValue('Asia/Ho_Chi_Minh')
+  asiaHoChiMinh,
+
+  @JsonValue('Asia/Hong_Kong')
+  asiaHongKong,
+
+  @JsonValue('Asia/Jakarta')
+  asiaJakarta,
+
+  @JsonValue('Asia/Jerusalem')
+  asiaJerusalem,
+
+  @JsonValue('Asia/Kathmandu')
+  asiaKathmandu,
+
+  @JsonValue('Asia/Kolkata')
+  asiaKolkata,
+
+  @JsonValue('Asia/Kuwait')
+  asiaKuwait,
+
+  @JsonValue('Asia/Muscat')
+  asiaMuscat,
+
+  @JsonValue('Asia/Qatar')
+  asiaQatar,
+
+  @JsonValue('Asia/Riyadh')
+  asiaRiyadh,
+
+  @JsonValue('Asia/Seoul')
+  asiaSeoul,
+
+  @JsonValue('Asia/Shanghai')
+  asiaShanghai,
+
+  @JsonValue('Asia/Singapore')
+  asiaSingapore,
+
+  @JsonValue('Asia/Taipei')
+  asiaTaipei,
+
+  @JsonValue('Asia/Tehran')
+  asiaTehran,
+
+  @JsonValue('Asia/Tokyo')
+  asiaTokyo,
+
+  @JsonValue('Atlantic/Reykjavik')
+  atlanticReykjavik,
+
+  @JsonValue('Australia/ACT')
+  australiaACT,
+
+  @JsonValue('Australia/Adelaide')
+  australiaAdelaide,
+
+  @JsonValue('Australia/Brisbane')
+  australiaBrisbane,
+
+  @JsonValue('Australia/Perth')
+  australiaPerth,
+
+  @JsonValue('Australia/Sydney')
+  australiaSydney,
+
+  @JsonValue('Europe/Amsterdam')
+  europeAmsterdam,
+
+  @JsonValue('Europe/Athens')
+  europeAthens,
+
+  @JsonValue('Europe/Belgrade')
+  europeBelgrade,
+
+  @JsonValue('Europe/Berlin')
+  europeBerlin,
+
+  @JsonValue('Europe/Brussels')
+  europeBrussels,
+
+  @JsonValue('Europe/Copenhagen')
+  europeCopenhagen,
+
+  @JsonValue('Europe/Dublin')
+  europeDublin,
+
+  @JsonValue('Europe/Helsinki')
+  europeHelsinki,
+
+  @JsonValue('Europe/Istanbul')
+  europeIstanbul,
+
+  @JsonValue('Europe/Lisbon')
+  europeLisbon,
+
+  @JsonValue('Europe/London')
+  europeLondon,
+
+  @JsonValue('Europe/Luxembourg')
+  europeLuxembourg,
+
+  @JsonValue('Europe/Madrid')
+  europeMadrid,
+
+  @JsonValue('Europe/Malta')
+  europeMalta,
+
+  @JsonValue('Europe/Moscow')
+  europeMoscow,
+
+  @JsonValue('Europe/Oslo')
+  europeOslo,
+
+  @JsonValue('Europe/Paris')
+  europeParis,
+
+  @JsonValue('Europe/Riga')
+  europeRiga,
+
+  @JsonValue('Europe/Rome')
+  europeRome,
+
+  @JsonValue('Europe/Stockholm')
+  europeStockholm,
+
+  @JsonValue('Europe/Tallinn')
+  europeTallinn,
